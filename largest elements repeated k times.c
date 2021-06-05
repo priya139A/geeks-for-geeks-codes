a5b2273616414cfaa0c7a6e386eeeb4398f8a203
priya139A <83228321+priya139A@users.noreply.github.com>
2021-06-05 12:39:21 +0530

    Create largest elements repeated k times.c

---
+++ largest elements repeated k times.c
@@ -0,0 +1,36 @@
+/*Smallest element repeated exactly ‘k’ times (not limited to small range)*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int duplicates(int n,int k,int arr[]){
+    map<int,int>mp;
+    int temp=INT_MIN;
+    //cout << k << endl;
+    for(int i=0;i<n;i++)
+        mp[arr[i]]++;
+    //cout << mp.size() << endl;
+    map<int, int>::iterator itr;
+    for(auto itr=mp.begin();itr!=mp.end();itr++){
+        //cout << itr->first <<" "<< itr->second << endl;
+        if(itr->second == k){
+            if(itr->first > temp){
+                temp=itr->first;
+            }
+        }
+    }
+    return temp;
+}
+    
+int main(){
+    int n,k,i,temp;
+    cin >> n >> k;
+    int arr[n];
+    for(i=0;i<n;i++){
+        cin >> temp;
+        arr[i]=temp;
+    }
+    //cout << k << endl;
+    temp=duplicates(n,k,arr);
+    cout << temp << endl;
+    return 0;
+}

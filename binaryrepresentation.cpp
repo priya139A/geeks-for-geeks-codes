2268c94bb7d16c5096f124751df45c65a17e5c0c
priya139A <83228321+priya139A@users.noreply.github.com>
2021-08-26 17:23:18 +0530

    Create binaryrepresentation.cpp

---
+++ binaryrepresentation.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+	int t,n;
+	cin >> t;
+	for(int i=0;i<t;i++){
+	    cin >> n;
+	    bitset<8>b(n);
+	    cout << b << endl;
+	}
+	return 0;
+}
